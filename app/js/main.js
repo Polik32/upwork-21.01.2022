40a15647f050d75f4b85ff50fcab362fb9053899
Polik32 <polik19195@gmail.com>
2022-02-07 16:50:12 +0200

    07.02.2022 work

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,25 @@
 
-$(function() {
-  //прикрепляем клик по заголовкам acc-head
-	$('.availability__title-wrap').on('click', f_acc);
-});
+// $(function() {
+//   //прикрепляем клик по заголовкам acc-head
+// 	$('.availability__title-wrap').on('click', f_acc);
+// });
  
-function f_acc(){
-//скрываем все кроме того, что должны открыть
-  // $('.availability__item-info').not($(this).next());
-// открываем или скрываем блок под заголовком, по которому кликнули
+// function f_acc(){
+// //скрываем все кроме того, что должны открыть
+//   // $('.availability__item-info').not($(this).next());
+// // открываем или скрываем блок под заголовком, по которому кликнули
     
-    $(this).next().slideToggle(500);
+//     $(this).next().slideToggle(500);
+// }
 
-  }
+$(document).ready(function () {
+
+  $('.availability__title-wrap').click(function () {
+    $(this).toggleClass('in').next().slideToggle(500);
+    $('.availability__title-wrap').not(this).removeClass('in').next().slideUp(500);
+      });
+
+  });
 
 //*****************************************************************/ 
 
